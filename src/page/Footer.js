c87857c32dfce57289cafac9381ea729c14f7211
sagarpatel6578 <sagar6578.sp@gmail.com>
2024-05-18 13:55:52 +0530

    footer

--- src/page/Footer.js
+++ src/page/Footer.js
@@ -1,180 +1,170 @@
-function Footer(){
-    return(<>
-     <footer id="site-footer" className="site-footer background four-columns">
-    <div className="footer">
-      <div className="section-padding">
-        <div className="section-container">
-          <div className="block-widget-wrap">
-            <div className="row">
-              <div className="col-lg-3 col-md-6 column-1">
-                <div className="block block-menu m-b-20">
-                  <h2 className="block-title">Contact Us</h2>
-                  <div className="block-content">
-                    <ul>
-                      <li>
-                        <span>Head Office:</span> 26 Wyle Cop, Shrewsbury,
-                        Shropshire, SY1 1XD
-                      </li>
-                      <li>
-                        <span>Tel:</span> 01743 234500
-                      </li>
-                      <li>
-                        <span>Email:</span>{" "}
-                        <a href="mailto:support@mojuri.com">
-                          support@mojuri.com
-                        </a>
-                      </li>
-                    </ul>
+import React from 'react';
+import SocialIcons from './SocialIcons';
+
+function Footer() {
+  const headerTitleStyle = {
+    fontSize: '18px',
+    fontFamily: 'Poppins, sans-serif',
+    fontWeight: 'bold',
+    textAlign: 'left',
+    color: '#cb8161'
+  };
+
+  const footerContentStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+  };
+
+  return (
+    <>
+      <footer id="site-footer" className="site-footer background four-columns">
+        <div className="footer">
+          <div className="section-padding">
+            <div className="section-container">
+              <div className="block-widget-wrap">
+                <div className="row">
+                  <div className="col-lg-3 col-md-6 column-1">
+                    <div className="block block-menu m-b-20">
+                      <h2 className="block-title" style={headerTitleStyle}>Contact Us</h2>
+                      <div className="block-content">
+                        <ul style={{ fontSize: '16px', fontFamily: 'Poppins, sans-serif' }}>
+                          <li>
+                            <span>Head Office:</span> 26 Wyle Cop, Shrewsbury,
+                            Shropshire, SY1 1XD
+                          </li>
+                          <li>
+                            <span>Tel:</span> 01743 234500
+                          </li>
+                          <li>
+                            <span>Email:</span>{" "}
+                            <a href="mailto:support@mojuri.com">
+                              support@mojuri.com
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                    <SocialIcons />
                   </div>
-                </div>
-                <div className="block block-social">
-                  <ul className="social-link">
-                    <li>
-                      <a href="#">
-                        <i className="fa fa-twitter" />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i className="fa fa-instagram" />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i className="fa fa-dribbble" />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i className="fa fa-behance" />
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <div className="col-lg-3 col-md-6 column-2">
-                <div className="block block-menu">
-                  <h2 className="block-title">Customer Services</h2>
-                  <div className="block-content">
-                    <ul>
-                      <li>
-                        <a href="shop-grid-left.html">Contact Us</a>
-                      </li>
-                      <li>
-                        <a href="shop-grid-left.html">Track Your Order</a>
-                      </li>
-                      <li>
-                        <a href="shop-grid-left.html">
-                          Product Care &amp; Repair
-                        </a>
-                      </li>
-                      <li>
-                        <a href="shop-grid-left.html">Book an Appointment</a>
-                      </li>
-                      <li>
-                        <a href="shop-grid-left.html">
-                          Frequently Asked Questions
-                        </a>
-                      </li>
-                      <li>
-                        <a href="shop-grid-left.html">
-                          Shipping &amp; Returns
-                        </a>
-                      </li>
-                    </ul>
+                  <div className="col-lg-3 col-md-6 column-2">
+                    <div className="block block-menu">
+                      <h2 className="block-title" style={headerTitleStyle}>Customer Services</h2>
+                      <div className="block-content">
+                        <ul style={{ fontSize: '16px', fontFamily: 'Poppins, sans-serif' }}>
+                          <li>
+                            <a href="shop-grid-left.html">Contact Us</a>
+                          </li>
+                          <li>
+                            <a href="shop-grid-left.html">Track Your Order</a>
+                          </li>
+                          <li>
+                            <a href="shop-grid-left.html">
+                              Product Care &amp; Repair
+                            </a>
+                          </li>
+                          <li>
+                            <a href="shop-grid-left.html">Book an Appointment</a>
+                          </li>
+                          <li>
+                            <a href="shop-grid-left.html">
+                              Frequently Asked Questions
+                            </a>
+                          </li>
+                          <li>
+                            <a href="shop-grid-left.html">Shipping &amp; Returns</a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
-              <div className="col-lg-3 col-md-6 column-3">
-                <div className="block block-menu">
-                  <h2 className="block-title">About Us</h2>
-                  <div className="block-content">
-                    <ul>
-                      <li>
-                        <a href="#">About Us</a>
-                      </li>
-                      <li>
-                        <a href="#">FAQ</a>
-                      </li>
-                      <li>
-                        <a href="#">Our Producers</a>
-                      </li>
-                      <li>
-                        <a href="#">Sitemap</a>
-                      </li>
-                      <li>
-                        <a href="#">Terms &amp; Conditions</a>
-                      </li>
-                      <li>
-                        <a href="#">Privacy Policy</a>
-                      </li>
-                    </ul>
+                  <div className="col-lg-3 col-md-6 column-3">
+                    <div className="block block-menu">
+                      <h2 className="block-title" style={headerTitleStyle}>About Us</h2>
+                      <div className="block-content">
+                        <ul style={{ fontSize: '16px', fontFamily: 'Poppins, sans-serif' }}>
+                          <li>
+                            <a href="#">About Us</a>
+                          </li>
+                          <li>
+                            <a href="#">FAQ</a>
+                          </li>
+                          <li>
+                            <a href="#">Our Producers</a>
+                          </li>
+                          <li>
+                            <a href="#">Sitemap</a>
+                          </li>
+                          <li>
+                            <a href="#">Terms &amp; Conditions</a>
+                          </li>
+                          <li>
+                            <a href="#">Privacy Policy</a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
-              <div className="col-lg-3 col-md-6 column-4">
-                <div className="block block-menu">
-                  <h2 className="block-title">Catalog</h2>
-                  <div className="block-content">
-                    <ul>
-                      <li>
-                        <a href="shop-grid-left.html">Earrings</a>
-                      </li>
-                      <li>
-                        <a href="shop-grid-left.html">Necklaces</a>
-                      </li>
-                      <li>
-                        <a href="shop-grid-left.html">Bracelets</a>
-                      </li>
-                      <li>
-                        <a href="shop-grid-left.html">Rings</a>
-                      </li>
-                      <li>
-                        <a href="shop-grid-left.html">Jewelry Box</a>
-                      </li>
-                      <li>
-                        <a href="shop-grid-left.html">Studs</a>
-                      </li>
-                    </ul>
+                  <div className="col-lg-3 col-md-6 column-4">
+                    <div className="block block-menu">
+                      <h2 className="block-title" style={headerTitleStyle}>Catalog</h2>
+                      <div className="block-content">
+                        <ul style={{ fontSize: '16px', fontFamily: 'Poppins, sans-serif' }}>
+                          <li>
+                            <a href="shop-grid-left.html">Earrings</a>
+                          </li>
+                          <li>
+                            <a href="shop-grid-left.html">Necklaces</a>
+                          </li>
+                          <li>
+                            <a href="shop-grid-left.html">Bracelets</a>
+                          </li>
+                          <li>
+                            <a href="shop-grid-left.html">Rings</a>
+                          </li>
+                          <li>
+                            <a href="shop-grid-left.html">Jewelry Box</a>
+                          </li>
+                          <li>
+                            <a href="shop-grid-left.html">Studs</a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
-    <div className="footer-bottom">
-      <div className="section-padding">
-        <div className="section-container">
-          <div className="block-widget-wrap">
-            <div className="row">
-              <div className="col-md-6">
-                <div className="footer-left">
-                  <p className="copyright">
-                    Copyright © 2023. All Right Reserved
-                  </p>
-                </div>
-              </div>
-              <div className="col-md-6">
-                <div className="footer-right">
-                  <div className="block block-image">
-                    <img
-                      width={309}
-                      height={32}
-                      src="media/payments.png"
-                      alt=""
-                    />
+        <div
+          className="footer-bottom"
+          style={{ fontSize: '14px', fontFamily: 'Poppins, sans-serif' }}>
+          <div className="section-padding">
+            <div className="section-container">
+              <div className="block-widget-wrap">
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="footer-left">
+                      <p className="copyright" style={{ textAlign: 'left', fontFamily: 'Poppins, sans-serif' }}>
+                        Copyright © 2024. All Right Reserved
+                      </p>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="footer-right">
+                      <div className="block block-image" style={{ textAlign: 'right' }}>
+                        <img width={309} height={32} src="media/payments.png" alt="" />
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
-  </footer>
-    </>)
+      </footer>
+    </>
+  );
 }
 
-export default Footer
+export default Footer;
---
+++ src/page/SocialIcons.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import '@fortawesome/fontawesome-free/css/all.css'; // Import the Font Awesome CSS
+// Make sure you have the correct path to the CSS file if it's not in the node_modules folder
+
+function SocialIcons() {
+  const iconStyle = {
+    fontSize: '24px',
+    color: '#cb8161', // Default font color
+    transition: 'color 0.3s ease', // Transition effect for smooth color change
+  };
+
+  const handleMouseEnter = (event) => {
+    event.target.style.color = '#000'; // Change color to black on hover
+  };
+
+  const handleMouseLeave = (event) => {
+    event.target.style.color = '#cb8161'; // Restore default color when mouse leaves
+  };
+
+  return (
+    <div className="block block-social">
+      <ul className="social-link">
+        <li>
+          <a href="#">
+            <i
+              className="fab fa-twitter"
+              style={iconStyle}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            ></i>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i
+              className="fab fa-instagram"
+              style={iconStyle}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            ></i>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i
+              className="fab fa-dribbble"
+              style={iconStyle}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            ></i>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i
+              className="fab fa-behance"
+              style={iconStyle}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            ></i>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default SocialIcons;
